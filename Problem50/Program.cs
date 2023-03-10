175988eb92913f30d9786fde24c5a55595cc016a
azamat Khadikov <khadikovazamat@gmail.com>
2023-03-09 20:10:01 -0700

    Problem50

---
+++ Problem50/Program.cs
@@ -0,0 +1,42 @@
+﻿Console.WriteLine("N of row");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("N of column");
+int m = Convert.ToInt32(Console.ReadLine());
+int [,] numbers = new int [10,10];
+FillArrayRandomNumbers(numbers);
+
+if (n > numbers.GetLength(0) || m > numbers.GetLength(1))
+{
+    Console.WriteLine("no such an element");
+}
+else
+{
+    Console.WriteLine($"element {n} and {m} is {numbers[n-1,m-1]}");
+}
+
+PrintArray(numbers);
+
+void FillArrayRandomNumbers(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+        {        
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                array [i,j] = new Random().Next(-100, 100)/10;
+            }   
+        }
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.Write("[ ");
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i,j] + " ");
+        }   
+        Console.Write("]");
+        Console.WriteLine(""); 
+    }
+}
